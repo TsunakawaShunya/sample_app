b740c8edf44ce78b03810d0f0125165159d267c6
Tsunakawa Shunya <138278662+TsunakawaShunya@users.noreply.github.com>
2024-07-01 12:40:54 +0000

    11.2

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -29,10 +29,10 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                   password_confirmation: "password" } }
     end
     follow_redirect!
-    assert_template 'users/show'
+    #assert_template 'users/show'
     #assert is_logged_in?
-    assert_select 'div.alert'
-    assert_select 'div.alert-success'
+    #assert_select 'div.alert'
+    #assert_select 'div.alert-success'
     assert_not flash.empty?
   end
 end
