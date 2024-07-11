075fa2f42949babbf1dbdf71a55573db1afef621
Tsunakawa Shunya <138278662+TsunakawaShunya@users.noreply.github.com>
2024-07-11 06:13:21 +0000

    13.3.1

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -56,23 +56,14 @@ class UsersController < ApplicationController
 
     # beforeフィルタ
 
-    # ログイン済みのユーザーかどうか確認
-    def logged_in_user
-      unless logged_in?
-        store_location
-        flash[:danger] = "Please log in."
-        redirect_to login_url, status: :see_other
-      end
+    # 正しいユーザーかどうか確認
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url, status: :see_other) unless current_user?(@user)
     end
 
-  # 正しいユーザーかどうか確認
-  def correct_user
-    @user = User.find(params[:id])
-    redirect_to(root_url, status: :see_other) unless current_user?(@user)
-  end
-
-  # 管理者かどうか確認
-  def admin_user
-    redirect_to(root_url, status: :see_other) unless current_user.admin?
-  end
+    # 管理者かどうか確認
+    def admin_user
+      redirect_to(root_url, status: :see_other) unless current_user.admin?
+    end
 end
